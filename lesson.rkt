959d4400b38fd95e7a032bd32463d164884d79d3
yikezawa <y.ikezawa93@gmail.com>
2022-06-27 00:01:14 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,2 +1,28 @@
 #lang racket
-(+ 5 3 4)
+(/ 6 2)
+(+ (* 2 4) (- 4 8))
+(define a 3)
+(define b (+ a 1))
+(= a b)
+(if (and (> b a)(< b (* a b)))
+         b
+         a)
+
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+(+ 2(if (> b a) b a))
+
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
+
+(/ (* 3 (- 6 2)(- 2 7))(+ (+ 5 4)(- 2(- 3 (+ 6 (/ 4 5))))))
+
+(define (func a b c)
+  (cond ((and (>= b a) (>= c a)) (+ (* b b) (* c c)))
+        ((and (>= a b) (>= c b)) (+ (* a a) (* b b)))
+        ((and (>= a c) (>= b c)) (+ (* a a) (* c c)))
+        )
+  )
