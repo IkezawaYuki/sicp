b4c5d496acea060bfcd444f6f7f7d26f686d8fa4
yikezawa <y.ikezawa93@gmail.com>
2022-06-30 00:21:48 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,14 +1,10 @@
 #lang racket
-(define (inc a) (+ a 1))
-(define (dec a) (- a 1))
 
-(define (+ a b)
-  (if (= a 0)
-      b
-      (inc (+ (dec a) b))))
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
 
-
-
-
-
-
+(define (f n)(A 0 n))
