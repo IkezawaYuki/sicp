553e16ab787312d0a5a1fdfda0741632d027a2ad
yikezawa <y.ikezawa93@gmail.com>
2022-07-11 23:30:02 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,186 +1,5 @@
 #lang racket
 
-(define (square x) (* x x))
-
-(define (expmod base exp m)
-  (cond ((= exp 0) 1)
-        ((even? exp)
-         (remainder (square (expmod base (/ exp 2) m))
-                    m))
-        (else
-         (remainder (* base (expmod base (- exp 1) m))
-                    m))))
-
-(define (carmichael-test n) ;;returns n-1 for carmichael number n
-  (define (ctest a i)
-    (cond ((= a n) i)
-          ((= (expmod a n n) a) (ctest (+ a 1) (+ i 1)))
-          (else (ctest (+ a 1) i))))
-  (ctest 1 0))
-
-(define (cube x) (* x x x))
-
-(define (sum-integers a b)
-  (if (> a b)
-      0
-      (+ a (sum-integers (+ a 1) b))))
-
-(define (sum-cubes a b)
-  (if (> a b)
-      0
-      (+ (cube a) (sum-cubes (+ a 1) b))))
-
-(define (pi-sum a b)
-  (if (> a b)
-      0
-      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
-
-(define (inc c)(+ c 1))
-
-(define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next a) next b))))
-
-(define (sum-cubes2 a b)
-  (sum cube a inc b))
-
-(define (identity x) x)
-
-(define (sum-integers2 a b)
-  (sum identity a inc b))
-
-(define (pi-sum2 a b)
-  (define (pi-term x)
-    (/ 1.0 (* x (+ x 2))))
-  (define (pi-next x)
-    (+ x 4))
-  (sum pi-term a pi-next b))
-
-
-(define (integral f a b dx)
-  (define (add-dx x) (+ x dx))
-    (* (sum f (+ a (/ dx 2.0)) add-dx b)
-         dx))
-
-(define (simpson f a b n)
-  (define h (/ (- b a) n))
-  (define (simpson-term x) (+ (f x)
-                              (* 4 (f (+ x h)))
-                              (f (+ x h h))))
-  (define (simpson-next x) (+ x h h))
-  (/ (* h (sum simpson-term a simpson-next (- b h)))
-     3.0))
-
-(define (sum2 term a next b)
-  (define (iter a result)
-    (if (> a b)
-        result
-        (iter (next a) (+ (term a) result))))
-  (iter a 0))
-
-(define (accumulate combiner null-value term a next b)
-  (if (> a b)
-      null-value
-      (combiner (term a)
-                (accumulate combiner null-value term (next a) next b))))
-
-(define (sum3 term a next b)
-  (accumulate + 0 term a next b))
-
-(define (product3 factor a next b)
-  (accumulate * 1 factor a next b))
-
-(define (accumulate2 combiner null-value term a next b)
-  (define (accumulate-iter a result)
-    (if (> a b)
-        result
-        (accumulate-iter (next a) (combiner (term a) result))))
-  (accumulate-iter a null-value))
-
-(define (pi-sum3 a b)
-  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
-       a
-       (lambda (x) (+ x 4))
-       b))
-
-(define (intergral f a b dx)
-  (* (sum f
-          (+ a (/ dx 2.0))
-          (lambda (x) (+ x dx))
-          b)
-     dx))
-
-(define (plus4 x) (+ x 4))
-
-(define plus5 (lambda (x) (+ x 5)))
-
-((lambda (x y z) (+ x y (square z))) 1 2 3)
-
-(define (f x y)
-  (define (f-helper a b)
-    (+ (* x (square a))
-       (* y b)
-       (* a b)))
-    (f-helper (+ 1 (* x y))
-              (- 1 y)))
-
-(define (f2 x y)
-  ((lambda (a b)
-     (+ (* x (square a))
-        (* y b)
-        (* a b)))
-   (+ 1 (* x y))
-   (- 1 y)))
-
-(define (f3 x y)
-  ((lambda (a b)
-     (+ (* x (square a))
-        (* y b)
-        (* a b)))
-   (+ 1 (* x y))
-   (- 1 y)))
-
-
-(define (f4 x y)
-  (let ((a (+ 1 (* x y)))
-        (b (- 1 y)))
-    (+ (* x (square a))
-       (* y b)
-       (* a b))))
-
-
-
-(define (f31 g)
-  (g 2))
-
-
-
-(define (search f neg-point pos-point)
-  (let ((midpoint (average neg-point pos-point)))
-    (if (close-enough? neg-point pos-point)
-        midpoint
-        (let ((test-value (f midpoint)))
-          (cond ((positive? test-value)
-                 (search f neg-point midpoint))
-                ((negative? test-value)
-                 (search f midpoint pos-point))
-                (else midpoint))))))
-
-(define (close-enough? x y)
-  (< (abs (- x y)) 0.001))
-
-(define (half-interval-method f a b)
-  (let ((a-value (f a))
-        (b-value (f b)))
-    (cond ((and (negative? a-value) (positive? b-value))
-           (search f a b))
-          ((and (negative? b-value) (positive? a-value))
-           (search f b a))
-          (else
-           (error "Value are not of opposite sign" a b)))))
-
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
@@ -193,9 +12,45 @@
           (try next))))
   (try first-guess))
 
+(fixed-point (lambda (x)(+ 1.0(/ 1.0 x))) 1.0)
 
+(define tolerance2 0.0001)
 
+(define (cont-frac n d k)
+  (define (cf i)
+    (if (= i k)
+        (/ (n i)(d i))
+        (/ (n i)(+ (d i)(cf (+ i 1))))))
+  (cf 1))
 
+(cont-frac-iter (lambda(i) 1.0)(lambda(i) 1.0) 12)
+
+(define (cont-frac n d k)
+  (define (cf res i)
+    (if (= i 0)
+        res
+        (cf (/ (n i)(+ (d i) res))(- i 1))))
+  (cf (/ (n k)(d k))(- k 1)))
+
+(+ 2.0
+   (cont-frac (lambda(i) 1.0)
+              (lambda(i)(if (= (remainder i 3) 2)
+                            (/ (+ i i 2.0) 3.0)
+                            1.0))
+              20))
+
+(define (cont-frac n d k)
+  (define (cf res i)
+    (if (= i 0)
+        res
+        (cf (/ (n i)(+ (d i) res))(- i 1))))
+  (cf (/ (n k)(d k))(- k 1)))
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i)(if (= i 1) x (- (* x x))))
+             (lambda (i)(+ i i -1.0)) k))
+
+(tan-cf (/ 3.14159 4) 10)
 
 
 
