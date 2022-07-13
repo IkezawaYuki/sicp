ad6066e577bbd180f12f828a0cb29805de30e23b
yikezawa <y.ikezawa93@gmail.com>
2022-07-13 23:49:16 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,5 +1,16 @@
 #lang racket
 
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda(x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda(x)
+    (- x(/ (g x)((deriv g) x)))))
+
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
@@ -12,77 +23,55 @@
           (try next))))
   (try first-guess))
 
-(fixed-point (lambda (x)(+ 1.0(/ 1.0 x))) 1.0)
+(define (cubic a b c)
+  (lambda(x)(+ (* (+ (* (+ x a) x) b) x) c)))
 
-(define tolerance2 0.0001)
+(newtons-method (cubic 3 3 1) 1)
 
-(define (cont-frac n d k)
-  (define (cf i)
-    (if (= i k)
-        (/ (n i)(d i))
-        (/ (n i)(+ (d i)(cf (+ i 1))))))
-  (cf 1))
+(define (compose f g)
+  (lambda(x) (f (g x))))
 
-(cont-frac-iter (lambda(i) 1.0)(lambda(i) 1.0) 12)
+(define (square x) (* x x))
 
-(define (cont-frac1 n d k)
-  (define (cf res i)
-    (if (= i 0)
-        res
-        (cf (/ (n i)(+ (d i) res))(- i 1))))
-  (cf (/ (n k)(d k))(- k 1)))
+(define (repeated f n)
+  (if (= n 0)
+      (lambda (x) x)
+      (compose f (repeated f(- n 1)))))
 
-(+ 2.0
-   (cont-frac (lambda(i) 1.0)
-              (lambda(i)(if (= (remainder i 3) 2)
-                            (/ (+ i i 2.0) 3.0)
-                            1.0))
-              20))
+(define (oddp x)
+  (= (remainder x 2) 1))
 
-(define (cont-frac3 n d k)
-  (define (cf res i)
-    (if (= i 0)
-        res
-        (cf (/ (n i)(+ (d i) res))(- i 1))))
-  (cf (/ (n k)(d k))(- k 1)))
+(define (repeated f n)
+  (if (= n 0)
+      (lambda (x) x)
+      (compose f(repeated f(- n 1)))))
 
-(define (tan-cf x k)
-  (cont-frac (lambda (i)(if (= i 1) x (- (* x x))))
-             (lambda (i)(+ i i -1.0)) k))
+(define dx 0.1)
 
-(tan-cf (/ 3.14159 4) 10)
+(define (smooth f)
+  (lambda (x) (/ (+ (f x)(f (- x dx))(f (+ x dx))) 3)))
+
+(define (n-fold-smooth f n)
+  ((repeated smooth n) f))
+
+(define (f x)
+  (let ((ix (floor x)))
+    (if (oddp ix)(+ (- ix x) 1)(-x ix))))
+
+(do ((x 5(+ x 1)))((= x 25))
+  (display (list (/ x 10)(f (/ x 10)))) (newline))
+
+(define k 1)
+
+(define (compose f g)
+  (lambda (x)(f (g x))))
+
+(define (repeated f n)
+  (if (= n 0)
+      (lambda(x) x)
+      (compose f (repeated f (- n 1)))))
 
 
-(define (average-damp f)
-  (lambda(x)(average x (f x))))
-
-(define (sqrt x)
-  (fixed-point (average-damp (lambda (y)(/ x y)))
-               1.0))
 
 
-(define (cube-root x)
-  (fixed-point (average-damp (lambda (y)(/ x(square y))))
-               1.0))
 
-(define dx 0.0001)
-
-(define (deriv g)
-  (lambda (x)
-    (/ (- (g (+ x dx)) (g x))
-       dx)))
-
-(define (cube x) (* x x x))
-
-((derive cube) 5)
-
-(define (newton-transform g)
-  (lambda(x)
-    (- x (/ (g x) ((deriv g) x)))))
-
-(define (newtons-method g guess)
-  (fixed-point (newton-transform g) guess))
-
-(define (sqrt x)
-  (newtons-method (lambda (y)(- (square y) x))
-                  1.0))
