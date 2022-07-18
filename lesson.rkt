3cc3a6aae2ed804b37279bf4c6a6e20542fe3408
yikezawa <y.ikezawa93@gmail.com>
2022-07-18 23:51:08 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -68,5 +68,28 @@
 
 (print-rat (add-rat one-half one-third))
 
+(define (make-point x y) (cons x y))
+(define (x-point p)(car p))
+(define (y-point p)(cdr p))
+
+(define (make-segment s e)(cons s e))
+(define (start-segment seg)(car seg))
+(define (end-segment seg)(cdr seg))
+
+(define (midpoint-segment seg)
+  (define (average a b)(/ (+ a b) 2))
+  (make-point
+   (average (x-point (start-segment seg))
+            (x-point (end-segment seg)))
+   (average (y-point (start-segment seg))
+            (y-point (end-segment seg)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
 
 
