8e3a7876063a1a5f58c24b52766c857ddeeb8c2e
yikezawa <y.ikezawa93@gmail.com>
2022-07-24 22:58:22 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -33,9 +33,56 @@
       0))
 
 
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-internal x y)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                 (- (upper-bound x) (upper-bound y))))
+
+let a = 4
+let b = 5
+
+(define a 4)
+(define b 5)
+(define c (+ a b))
 
 
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
 
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b)(cons a b))
+(define (upper-bound i) (cdr i))
+(define (lower-bound i) (car i))
+
+(define (make-center-width w)
+  (make-interval (- c w) (+ c w)))
+
+(define (percent i)
+  (* (/ (- (upper-bound i)(lower-bound i)) (center i)) 100))
+
+(define (center i)
+  (/ (+ (upper-bound i) (lower-bound i)) 2))
 
 
 
