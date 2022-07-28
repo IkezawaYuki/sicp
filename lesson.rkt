d4329a4276bea78330f8d3d9a969277172e006d6
yikezawa <y.ikezawa93@gmail.com>
2022-07-28 23:45:14 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -16,3 +16,18 @@
       0
       (+ 1 (length (cdr items)))))
 
+(define odds (list 1 3 5 7))
+
+(length odds)
+
+(define (length2 items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1)list2))))
