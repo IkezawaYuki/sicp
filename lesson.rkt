f4df28d7d77d335955f826c6ceca742ba7070ab0
yikezawa <y.ikezawa93@gmail.com>
2022-07-30 23:54:42 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -45,3 +45,13 @@
   (cons first (sp first rest)))
 
 
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
