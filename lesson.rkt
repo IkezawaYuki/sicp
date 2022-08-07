73279449ab84be81da16a12b32a2c49c48d7c59a
yikezawa <y.ikezawa93@gmail.com>
2022-08-07 23:44:48 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,18 +1,27 @@
 #lang racket
 
-(define (scale-tree tree factor)
-  (cond ((null? tree) null)
-        ((not (pair? tree)) (* tree factor))
-        (else (cons (scale-tree (car tree) factor)
-                    (scale-tree (cdr tree) factor)))))
 
-(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((pair? tree) (cons (square-tree (car tree))
+                            (square-tree (cdr tree))))
+        (else (* tree tree))))
 
-(define (scale-tree2 tree factor)
-  (map (lambda (sub-tree)
-         (if (pair? sub-tree)
-             (scale-tree2 sub-tree factor)
-             (* sub-tree factor)))
-       tree))
 
-(scale-tree2 (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+(square-tree (list 1
+                   (list 2 (list 3 4) 5)
+                   (list 6 7)))
+
+
+(define (square-tree tree)
+  (if (pair? tree)
+      (map (lambda (x) (square-tree x)) tree)
+      (* tree tree)))
+
+
+(define (tree-map f tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (f tree))
+        (else (cons (tree-map f (car tree))
+                    (tree-map f (cdr tree))))))
+
