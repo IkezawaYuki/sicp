b678a203bf6c1056c0001c94e226bbbc0b3d890f
yikezawa <y.ikezawa93@gmail.com>
2022-08-11 23:32:15 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -57,3 +57,10 @@
          (cons (car sequence)
                (filter predicate (cdr sequence))))
         (else (filter predicate (cdr sequence)))))
+
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
