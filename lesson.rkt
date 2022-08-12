f693b5dde135758ce8369b3c2967c3a3b6306d1d
yikezawa <y.ikezawa93@gmail.com>
2022-08-12 23:31:56 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,66 +1,26 @@
 #lang racket
 
-
-(define (square-tree tree)
-  (cond ((null? tree) '())
-        ((pair? tree) (cons (square-tree (car tree))
-                            (square-tree (cdr tree))))
-        (else (* tree tree))))
-
-
-(square-tree (list 1
-                   (list 2 (list 3 4) 5)
-                   (list 6 7)))
-
-
-(define (square-tree2 tree)
-  (if (pair? tree)
-      (map (lambda (x) (square-tree x)) tree)
-      (* tree tree)))
-
-
-(define (tree-map f tree)
-  (cond ((null? tree) '())
-        ((not (pair? tree)) (f tree))
-        (else (cons (tree-map f (car tree))
-                    (tree-map f (cdr tree))))))
-
-(define (subsets s)
-  (if (null? s)
-      (list '())
-      (let ((rest (subsets (cdr s))))
-        (append rest (map (lambda (x) (cons (car s) x))
-                          rest)))))
-
-(define (sum-odd-squares tree)
-  (cond ((null? tree) 0)
-        ((not (pair? tree))
-         (if (odd? tree) (square tree) 0))
-        (else (+ (sum-odd-squares (car tree))
-                 (sum-odd-squares (cdr tree))))))
-
-(define (even-fibs n)
-  (define (next k)
-    (if (> k n)
-        null
-        (let ((f (fib k)))
-          (if (even? f)
-              (cons f (next (+ k 1)))
-              (next (+ k 1))))))
-  (next 0))
-
-(map square (list 1 2 3 4 5))
-
-(define (filter predicate sequence)
-  (cond ((null? sequence) nil)
-        ((predicate (car sequence))
-         (cons (car sequence)
-               (filter predicate (cdr sequence))))
-        (else (filter predicate (cdr sequence)))))
-
-
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
+
+(accumulate + 0 (list 1 2 3 4 5))
+
+(accumulate * 1 (list 1 2 3 4 5))
+
+(define (enumurate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumurate-interval (+ low 1) high))))
+
+(enumurate-interval 2 8)
+
+(define (enumurate-tree tree)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumurate-tree (car tree))
+                      (enumurate-tree (cdr tree))))))
+
+(enumurate-tree (list 1 (list 2 (list 3 4)) 5))
