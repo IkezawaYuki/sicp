90b3a4656a236e366ae1f68c3e93daebeeb9e503
yikezawa <y.ikezawa93@gmail.com>
2022-08-14 23:46:44 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -1,34 +1,20 @@
 #lang racket
 
-(define (accumulate op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
-
-(accumulate + 0 (list 1 2 3 4 5))
-
-(accumulate * 1 (list 1 2 3 4 5))
-
-(define (enumurate-interval low high)
-  (if (> low high)
-      null
-      (cons low (enumurate-interval (+ low 1) high))))
-
-(enumurate-interval 2 8)
-
-(define (enumurate-tree tree)
-  (cond ((null? tree) null)
-        ((not (pair? tree)) (list tree))
-        (else (append (enumurate-tree (car tree))
-                      (enumurate-tree (cdr tree))))))
-
-(enumurate-tree (list 1 (list 2 (list 3 4)) 5))
-
-
-(define (even-fibs n)
+(define (list-fib-squares n)
   (accumulate cons
               null
-              (filter even?
-                      (map fib
-                           (enumerate-inteval 0 n)))))
+              (map square
+                   (map fib
+                        (enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+              1
+              (map square
+                   (filter odd? sequence))))
+
+(define (salary-of-highest-paid-programmer records)
+  (accumulate max
+              0
+              (map salary
+                   (filter programmer? records))))
