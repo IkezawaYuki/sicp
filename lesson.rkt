a4999f9f8e0bc9660dd3767a2808e56f83d72aea
yikezawa <y.ikezawa93@gmail.com>
2022-08-23 00:11:40 +0900

    mod

--- lesson.rkt
+++ lesson.rkt
@@ -39,3 +39,17 @@
       '()
       (cons (accumulate op init (map (car seqs))
                         (accumulate-n op init (map cdr seqs))))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product v x)) m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x)
+           (map (lambda (y)(dot-product x y)) cols)) m)))
