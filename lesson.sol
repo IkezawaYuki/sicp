1cfbf69d6ef09ce0fba1913bbb7f177837dda24b
yikezawa <y.ikezawa93@gmail.com>
2022-08-26 23:44:40 +0900

    mod

--- lesson.sol
+++ lesson.sol
@@ -1,9 +1,25 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 
-contract SimpleAuction {
-  address payable public beneficiary;
-  uint public auctionEndTime;
+contract BlindAuction {
+  struct Bid {
+    bytes32 blindedBid;
+    uint deposit;
+  }
+
+  address payable public beneficiay;
+  uint public biddingEnd;
+  uint public revealEnd;
+  bool public ended;
+
+  mapping(address => Bid[]) public bids;
+
+  address public highestBidder;
+  uint public highestBid;
+
+  mapping(address => uint) pendingReturns;
+
+  event AuctionEnded(address winner, uint highestBid);
 
   
 }
